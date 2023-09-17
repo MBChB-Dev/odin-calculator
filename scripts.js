fa5311c1f964f3ef34fa35b584548e8a2fed8678
David Kabanda <116374315+MBChB-Dev@users.noreply.github.com>
2023-09-17 18:48:17 +0200

    Create functions for add, subtract, multiply, divide

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
+// add
+// subtract
+// multiply
+// divide
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
