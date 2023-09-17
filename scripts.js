75df48639b99e2e2b77c0125235e3aa71739cc0b
David Kabanda <116374315+MBChB-Dev@users.noreply.github.com>
2023-09-17 18:51:57 +0200

    Create variables for first number, operator, and second number

--- scripts.js
+++ scripts.js
@@ -19,3 +19,10 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b;
 }
+
+
+// A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
+
+const operandA;
+const operandB;
+const operator;
